fa31bf1e231e4d6e1bf71a6eb3aa7424b0ccdb81
avigunjal07 <avishkargunjal07@gmail.com>
2026-04-30 22:53:45 +0530

    logic for correct option is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import './App.css'
+import questions from "./data";
+import { useState } from "react";
 
 const App = () => {
+
+   const [questionIndex, setQuestionIndex] = useState(0);
+  const [optionStyles, setOptionStyles] = useState({
+  0: {},
+  1: {},
+  2: {},
+  3: {},
+  });
+
+  const currentQuestion = questions[questionIndex];
+
   return (
     <div>
       

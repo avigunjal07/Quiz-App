119b4a305955aad0c546c069435ee7d7f0c14f58
avigunjal07 <avishkargunjal07@gmail.com>
2026-04-30 21:24:55 +0530

    Questions file is created as data.js

---
+++ src/data.js
@@ -0,0 +1,104 @@
+const questions = [
+  {
+    question: "Which language is used to style web pages?",
+    options: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Python"
+    ],
+    answer: "CSS",
+  },
+  {
+    question: "Which tag is used to create a hyperlink in HTML?",
+    options: [
+      "<h1>",
+      "<a>",
+      "<link>",
+      "<href>"
+    ],
+    answer: "<href>",
+  },
+  {
+    question: "Which symbol is used to end a statement in Java?",
+    options: [
+      ";",
+      ":",
+      ".",
+      ","
+    ],
+    answer: ";",
+  },
+  {
+    question: "Which data type is used to store decimal numbers in Java?",
+    options: [
+      "float",
+      "int",
+      "char",
+      "double"
+    ],
+    answer: "float",
+  },
+  {
+    question: "Which keyword is used to define a constant in Java?",
+    options: [
+      "const",
+      "define",
+      "static",
+      "final"
+    ],
+    answer: "const",
+  },
+  {
+    question: "Which property is used in CSS to change text color?",
+    options: [
+      "text-color",
+      "font-color",
+      "color",
+      "background-color"
+    ],
+    answer: "color",
+  },
+  {
+    question: "What does CSS stand for?",
+    options: [
+      "Computer Style Sheets",
+      "Creative Style System",
+      "Cascading Style Sheets",
+      "Colorful Style Sheets"
+    ],
+    answer: "Cascading Style Sheets",
+  },
+  {
+    question: "Which HTML tag is used to display an image?",
+    options: [
+      "<img>",
+      "<image>",
+      "<pic>",
+      "<src>"
+    ],
+    answer: "<img>",
+  },
+  {
+    question: "Which file extension is used for JavaScript files?",
+    options: [
+      ".java",
+      ".js",
+      ".html",
+      ".css"
+    ],
+    answer: ".js",
+  },
+  {
+    question: "Which loop is guaranteed to execute at least once?",
+    options: [
+      "do-while",
+      "while",
+      "for",
+      "if"
+    ],
+    answer: "do-while",
+  },
+];
+
+export default questions;
